6e7b91604f5d44ae95529e6422af27ab56acb153
EzequielMelo <ezequielanthonymelo@gmail.com>
2024-09-05 03:27:31 -0300

    Ejercicio 1 - finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ejercicios-clase-01';
+  edadUno: string = '';
+  edadDos: string = '';
+  promedio: string = '';
+  suma: string = '';
+
+  mostrarResultados() {
+    const numEdadUno = parseFloat(this.edadUno) || 0;
+    const numEdadDos = parseFloat(this.edadDos) || 0;
+    this.suma = (numEdadUno + numEdadDos).toString();
+    this.promedio = ((numEdadUno + numEdadDos) / 2).toString();
+  }
+
+  borrarDatos() {
+    this.edadUno = '';
+    this.edadDos = '';
+    this.promedio = '';
+    this.suma = '';
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { ErrorComponent } from './components/error/error.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'bienvenido', component: BienvenidoComponent },
+  { path: 'error', component: ErrorComponent },
+  { path: 'login', component: LoginComponent }
+];
